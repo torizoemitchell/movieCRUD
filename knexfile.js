c4a8f06fd251ab7f4c232468ef8beb1aa0cf6da2
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-10-19 09:51:35 -0600

    added database url

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,6 @@ development: {
 test: {},
 production: {
  client: 'pg',
- connection: process.env.DATABASE_URL
+ connection: 'postgres://potunkhdgajdbb:30b047c2327445987d76d816ff1cee35299e875a4a1a006f77f9017b4cd185f9@ec2-54-243-187-30.compute-1.amazonaws.com:5432/da6p3o4uihmg51'
 }
 }
