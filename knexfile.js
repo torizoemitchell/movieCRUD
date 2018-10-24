a1c4ef351e9938a5ed41617cf6928c29ebe07728
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-10-24 08:33:31 -0600

    added to package.json and knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,3 @@ production: {
  client: 'pg',
  connection: 'postgres://potunkhdgajdbb:30b047c2327445987d76d816ff1cee35299e875a4a1a006f77f9017b4cd185f9@ec2-54-243-187-30.compute-1.amazonaws.com:5432/da6p3o4uihmg51'
 }
-}
