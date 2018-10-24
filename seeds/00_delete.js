a3c0e27b871d99d203b549bdceeb6b90f8f90c53
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-10-24 09:53:10 -0600

    re-migrated and re-seeded

--- seeds/00_delete.js
+++ seeds/00_delete.js
@@ -1,7 +1,6 @@
 
 exports.seed = function (knex, Promise) {
     // Deletes ALL existing entries
-    return knex('directors_movies').del()
+    return knex('movies').del()
         .then(() => knex('movies').del())
-        .then(() => knex('directors').del())
 }
